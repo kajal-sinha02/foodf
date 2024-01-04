da1c5e088edc0a40b593a84d4bfbf3f9f80fd946
kajal-sinha02 <kajal.2022ug1049@iiitranchi.ac.in>
2024-01-04 19:07:57 +0530

    .

--- mernbackened/public/script/script.js
+++ mernbackened/public/script/script.js
@@ -1,25 +1,27 @@
 
-// JavaScript code to open a webpage when the button is clicked
 document.getElementById("sign_btn").addEventListener("click", function() {
-  // Replace 'https://www.example.com' with the URL of the webpage you want to open
-  window.location.href = '/register';
+    // Replace 'https://www.example.com' with the URL of the webpage you want to open
+    window.location.href = '/register';
+    
+  });
   
-});
+  document.getElementById("signin_btn").addEventListener("click", function() {
+    // Replace 'https://www.example.com' with the URL of the webpage you want to open
+    window.location.href = '/login';
+    
+  });
+  document.getElementById("donatenow").addEventListener("click", function() {
+    // Replace 'https://www.example.com' with the URL of the webpage you want to open
+    window.location.href = 'https://business.paytm.com/docs/api/create-link-api/';
+    window.open(url, '_blank');
+  });
 
-document.getElementById("signin_btn").addEventListener("click", function() {
-  // Replace 'https://www.example.com' with the URL of the webpage you want to open
-  window.location.href = '/login';
+  document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      // Show the delayed image after 5 seconds
+      document.getElementById('combined').style.display = 'block';
+    }, 1000); // 5000 milliseconds = 5 seconds
+  });
+
+
   
-});
-document.getElementById("donatenow").addEventListener("click", function() {
-  // Replace 'https://www.example.com' with the URL of the webpage you want to open
-  window.location.href = 'https://business.paytm.com/docs/api/create-link-api/';
-  window.open(url, '_blank');
-});
-
-
-
-
-console.log(username)
-
-
